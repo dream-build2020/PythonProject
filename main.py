b65d3fbffd515dfe71cdb4b87791c5d99fb705fe
kindtom123@gmail.com <kindtom123@gmail.com>
2025-07-30 23:25:50 +0800

    This is my first demo

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("This script is intended to be run as a module, not directly.")
+    print("Please use it as a module in your application.")
+    print("Exiting now.")
+    exit(1)
